93ccdcaa6453c984d81eb23a7464f0e562691da1
Dani-code-opsC <danielamoo35@gmail.com>
2023-07-11 23:49:50 +0100

    Ex 3-4.py

---
+++ Chapter3/Exercise3-4.py
@@ -0,0 +1,4 @@
+#Exercise 3-4
+guest_list =['tayo', 'loren', 'pedro']
+print(f"""you are invited {guest_list[0].title()}.\nyou are invited {guest_list[1].title()}.\
+      \nyou are invited {guest_list[-1].title()}""")
---
+++ Chapter3/Exercisise3-3.py
@@ -0,0 +1,6 @@
+#Exercise 3-3
+jets = ['f-100', 'g-250', '40xz']
+print(f"I would like to own a {jets[0].title()} one day, but a {jets[-1].upper()} is not a bad idea."
+      f"\nI just despise that {jets[1]}")
+#output - I would like to own a F-100 one day, but a 40XZ is not a bad idea.
+#I just despise that g-250
